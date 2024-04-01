f8d47648853fa4d654c3598a6223b36921465232
chanawh <thechanwh@gmail.com>
2024-03-31 22:07:23 -0700

    Included preOrderTraversal and postOrderTraversal methods

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -44,6 +44,24 @@ class BinarySearchTree {
       this.inOrderTraversal(currentNode.right, callback);
     }
   }
+
+    // Pre-order traversal: Visit root, then left subtree, then right subtree
+    preOrderTraversal(currentNode, callback) {
+      if (currentNode !== null) {
+        callback(currentNode.data);
+        this.preOrderTraversal(currentNode.left, callback);
+        this.preOrderTraversal(currentNode.right, callback);
+      }
+    }
+
+  // Post-order traversal: Visit left subtree, then right subtree, then root
+  postOrderTraversal(currentNode, callback) {
+    if (currentNode !== null) {
+      this.postOrderTraversal(currentNode.left, callback);
+      this.postOrderTraversal(currentNode.right, callback);
+      callback(currentNode.data);
+    }
+  }
 }
 
 // Example usage
@@ -58,3 +76,11 @@ tree.insert(7);
 const inOrderValues = [];
 tree.inOrderTraversal(tree.root, (data) => inOrderValues.push(data));
 console.log("In-order traversal:", inOrderValues); // Output: [3, 5, 7, 10, 15]
+
+const preOrderValues = [];
+tree.preOrderTraversal(tree.root, (data) => preOrderValues.push(data));
+console.log("Pre-order traversal:", preOrderValues); // Output: [10, 5, 3, 7, 15]
+
+const postOrderValues = [];
+tree.postOrderTraversal(tree.root, (data) => postOrderValues.push(data));
+console.log("Post-order traversal:", postOrderValues); // Output: [3, 7, 5, 15, 10]
